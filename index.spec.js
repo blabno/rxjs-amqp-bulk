c6d6661c3289b6c03321de4e70b0a90ffa6e4066
sajdakk <kristof.sajdak@gmail.com>
2016-02-25 16:24:32 +0100

    added a test case for buffering still buggy though, running 2 tests will freeze the observable somehow

--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,6 @@
 const chai = require('chai')
+const expect = chai.expect
+
 const Promise = require('bluebird')
 const jackrabbit = require('jackrabbit')
 const _ = require('lodash')
@@ -10,11 +12,7 @@ chai.config.includeStack = true
 
 const dockerHostName = url.parse(process.env.DOCKER_HOST).hostname
 const rabbit = jackrabbit(`amqp://${dockerHostName}:5672`)
-var exchange = rabbit.default()
-
-function publish(i) {
-    exchange.publish(i, {key: 'hello'})
-}
+const exchange = rabbit.default()
 
 function rabbitToObservable(consume) {
 
@@ -33,24 +31,53 @@ describe('Rxjs AMQP', ()=> {
 
     it('should convert a jackrabbit queue into an observable', (done)=> {
 
-        const queue = rabbit
-            .default()
-            .queue({name: 'hello'})
+        const queue = exchange.queue({name: 'hello'})
 
         const consume = _.partialRight(queue.consume, {noAck: true})
 
         rabbitToObservable(consume)
             .subscribe(
                 (event)=> {
-                    if (event.data == 9) {
+                    if (event.data === `4`) {
                         done()
                     }
                 },
-                done
-            )
+                (err) => {
+                    done(err)
+                }
+            );
 
-        _.range(10).forEach((i) => publish(i))
+        _.range(5).map((i)=> `${i}`).forEach((i) => exchange.publish(i, {key: 'hello'}))
+    })
 
+    it('should buffer messages', (done)=> {
+
+        const range = _.range(0, 5).map((i)=> `${i}`)
+
+        const queue = exchange.queue({name: 'hello'})
+
+        const consume = _.partialRight(queue.consume, {noAck: true})
+
+        rabbitToObservable(consume)
+            .bufferWithCount(5)
+            .subscribe(
+                (events)=> {
+
+                    const actual = _.map(events, 'data')
+                    const expected = range.slice(0, 5)
+
+                    expect(expected).to.eql(actual)
+
+                    done()
+
+                },
+                (err) => {
+                    done(err)
+                }
+            );
+
+        range.forEach((i) =>
+            exchange.publish(i, {key: 'hello'}))
     })
 
 })
