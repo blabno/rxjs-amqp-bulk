6d9d9d08f0685d70508cfddd653b661f420d60ea
sajdakk <kristof.sajdak@gmail.com>
2016-03-23 21:56:06 +0100

    added prefetch to prevent flooding memory

--- test/es-sync.spec.js
+++ test/es-sync.spec.js
@@ -121,7 +121,7 @@ describe('AMQP Elasticsearch bulk sync', ()=> {
 
         it('feeds amqp messages with associated channel information and an ack/nack shorthands into an observer', (done)=> {
 
-            const subscription = EsSync.esQueueConsumeObservable(global.amqpConnection)
+            const subscription = EsSync.esQueueConsumeObservable(global.amqpConnection, config)
                 .subscribe(
                     (event)=> {
                         event.ack()
