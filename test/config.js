f725abef28f9cb58aa99aaa7352632a1e799fdd5
sajdakk <kristof.sajdak@gmail.com>
2016-04-14 23:18:51 +0200

    simplified the docker setup switched to latest commit on hapi-harvester which doesn't require an adapterSSE

--- test/config.js
+++ test/config.js
@@ -8,7 +8,6 @@ module.exports = {
     appHostPort: 3000,
     appHostUrl: 'http://localhost:3000',
     mongodbUrl: `mongodb://${dockerHostName}/test`,
-    mongodbOplogUrl: `mongodb://${dockerHostName}/local`,
     amqpUrl : `amqp://${dockerHostName}:5672`,
     esHostUrl: `http://${dockerHostName}:9200`,
     bufferTimeout: 3000000,
